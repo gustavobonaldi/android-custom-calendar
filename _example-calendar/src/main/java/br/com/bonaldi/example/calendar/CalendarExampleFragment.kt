33e158f826528b25ad15818feac817eec0070fd0
Gustavo Bonaldi <gustavo.bonaldi@btgpactual.com>
2022-06-02 21:04:53 -0300

    feat(calendar): update example layout

---
+++ _example-calendar/src/main/java/br/com/bonaldi/example/calendar/CalendarExampleFragment.kt
@@ -0,0 +1,95 @@
+package br.com.bonaldi.example.calendar
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import br.com.bonaldi.customcalendar.helpers.DateHelper.getTodayDate
+import br.com.bonaldi.customcalendar.helpers.IntHelper.orZero
+import br.com.bonaldi.customcalendar.listeners.OnCalendarChangedListener
+import br.com.bonaldi.customcalendar.models.day.CalendarDay
+import br.com.bonaldi.customcalendar.models.enums.CalendarSelectionTypeEnum
+import br.com.bonaldi.example.calendar.databinding.FragmentCalendarExampleBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class CalendarExampleFragment : Fragment() {
+    private lateinit var binding: FragmentCalendarExampleBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCalendarExampleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupCalendarConfigOptions()
+    }
+
+    private fun setupCalendarComponent(selectionTypeEnum: CalendarSelectionTypeEnum = CalendarSelectionTypeEnum.SINGLE){
+        binding.customCalendarItem.apply {
+            setOnCalendarChangedListener(object : OnCalendarChangedListener {
+                override fun onSelectDates(list: List<CalendarDay>) {
+                    //TODO("Not yet implemented")
+                }
+
+                override fun onSelectDate(date: CalendarDay?) {
+                    //TODO("Not yet implemented")
+                }
+
+                override fun onMaxSelectionReach(selectedQuantity: Int) {
+                    Toast.makeText(
+                        context,
+                        "Quantidade máxima de $selectedQuantity já foi selecionada.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+            setMinDate(getTodayDate())
+            getTodayDate().apply {
+                year = year.orZero() + 1
+                setMaxDate(this)
+            }
+            setCalendarSelectionType(selectionTypeEnum)
+            refreshCalendar()
+        }
+    }
+
+    private fun setupCalendarConfigOptions() = binding.apply {
+        val selectionTypeAdapter = setupSpinnerAdapter(requireContext(), CalendarSelectionTypeEnum.values().map { it.name.toLowerCase().capitalize() }.toList())
+        spinnerSelectionType.adapter = selectionTypeAdapter
+        spinnerSelectionType.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                CalendarSelectionTypeEnum.values()[position].let { selectedType ->
+                    setupCalendarComponent(selectedType)
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
+    private fun <T> setupSpinnerAdapter(context: Context, itemList: List<T>): ArrayAdapter<T> {
+        val arrayAdapter: ArrayAdapter<T> = ArrayAdapter<T>(
+            context,
+            R.layout.calendar_example_spinner_item,
+            itemList
+        )
+        arrayAdapter.setDropDownViewResource(R.layout.calendar_example_spinner_dropdown_item)
+        return arrayAdapter
+    }
+}
